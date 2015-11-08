b5682074629f458d9c9fd70018f3f39511f6d7f2
Jack Sneed <sneed.jack@gmail.com>
2015-11-08 03:53:21 -0800

    add files .

--- time-serever.js
+++
@@ -1,9 +0,0 @@
-var net = require('net');
-var port = process.argv[2];
-var strftime = require('strftime');
-
-var server = net.createServer(function(socket){
-  var data = strftime('%Y-%m-%d %H:%M');
-  socket.end(data+'\n');
-});
-server.listen(port);
